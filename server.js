3d3590f3d51fa9925fb13849d0919eb5ae242978
System <none>
2017-06-21 21:43:09 +0000

    Implement /api/latest/imagesearch

--- server.js
+++ server.js
@@ -5,9 +5,11 @@
 var express = require('express');
 var url = require('url');
 var request = require('request');
+var mongo = require('mongodb').MongoClient;
 
 // Instantiations: A set of statements to create objects
 var app = express();
+const db_url = "mongodb://"+process.env.USER+":"+process.env.PASSWORD+"@ds153609.mlab.com:53609/rafascardb";
 
 // Configurations: A set of statements to configure system and custom settings
 app.use(express.static('public'));
@@ -23,8 +25,20 @@ app.get("/api/imagesearch/:term", function (req, res) {
   const term = req.params.term;
   const offset = url.parse(req.url, true).query.offset;
   
+  // Add search to database history
+  addToHistory(term);
+  
   searchImages(term, offset, function(err, images) {
     res.send(images);
+  });
+});
+
+app.get("/api/latest/imagesearch", function (req, res) {
+  // Get database history
+  getHistory(function(err, hist) {
+    if(err) throw err;
+    
+    res.send(hist);
   });
 });
 
@@ -73,4 +87,36 @@ function encodeQueryData(data) {
     ret.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
   }
   return ret.join('&');
+}
+
+function addToHistory(term) {
+  mongo.connect(db_url, function(err, db) {
+    if(err) throw err;
+    
+    const collection = db.collection("imagesearch");
+    collection.insert({
+      term: term,
+      date: new Date().toString()
+    }, function(err, doc) {
+      if(err) throw err;
+      db.close();
+    });
+  });
+}
+
+function getHistory(callback) {
+  mongo.connect(db_url, function(err, db) {
+    if(err) throw err;
+    
+    const collection = db.collection("imagesearch");
+    collection.find({}, {_id: false})
+      .sort({$natural:-1})
+      .limit(10)
+      .toArray(function(err, docs) {
+        if(err) callback(err);
+
+        db.close();
+        callback(null, docs);   
+      });
+  });
 }
